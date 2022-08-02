0cf68bfbd27431121cd99a2432948361da41d2f6
Joshua Cheung <contact.cjoshua@gmail.com>
2022-08-01 17:09:43 -0700

    complete home style

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import myPic from "../../image/me_2.jpg";
 import "./Home.scss";
 
 export default function Home() {
   return (
-    <section>
+    <section className="home_main">
       <h4 className="title_sub">Hello, I'm</h4>
       <h1 className="title_main">Joshua Cheung</h1>
       <h4 className="title_sub">Full-Stack Developer</h4>
       <div>
-        <img src="" alt="" />
+        <img src={myPic} alt="" className="myPic" />
       </div>
-      <div>
-        <button>Download CV</button>
-        <button>Contact me</button>
+      <div className="button_list">
+        <button className="btn line">Download CV</button>
+        <button className="btn solid">Contact me</button>
       </div>
     </section>
   );
