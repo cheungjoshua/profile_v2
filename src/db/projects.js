09328d23cd398e439223f215a2e140ea845a355d
Joshua Cheung <contact.cjoshua@gmail.com>
2022-09-20 14:19:01 -0700

    Add new project

--- src/db/projects.js
+++ src/db/projects.js
@@ -39,4 +39,14 @@ export const projects = [
     link: "https://github.com/cheungjoshua/movie-app",
     stack: "React, HTML, SASS, Axios, Javascript",
   },
+  {
+    id: 4,
+    name: "MERN Todo App",
+    image:
+      "https://github.com/cheungjoshua/mern-todo/raw/main/client/doc/Screen%20Shot%202022-09-20%20at%2014.02.43.png?raw=true",
+    describe:
+      "TODO app with MERN stack, user able to ADD, EDIT, DELETE, SEARCH Todo.",
+    link: "https://github.com/cheungjoshua/mern-todo",
+    stack: "React, Express, Node, MongoDB",
+  },
 ];
