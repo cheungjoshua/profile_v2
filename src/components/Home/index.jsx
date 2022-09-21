4fd35812e8eb9d589d2b277818cf7cef975e4124
Joshua Cheung <contact.cjoshua@gmail.com>
2022-09-21 13:45:49 -0700

    mobile version header and button layout complete

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,19 +6,21 @@ import "./Home.scss";
 export default function Home() {
   return (
     <section className="home_main" id="home">
-      <h4 className="title_sub">Hello, I'm</h4>
-      <h1 className="title_main">Joshua Cheung</h1>
-      <h4 className="title_sub">Full-Stack Developer</h4>
-      <div>
-        <img src={myPic} alt="" className="myPic" />
-      </div>
-      <div className="button_list">
-        <a href={Resume} download>
-          <button className="btn line">Download CV</button>
-        </a>
-        <a href="mailto:joshuacycheung@gmail.com">
-          <button className="btn solid">Contact me</button>
-        </a>
+      <div className="wrapper">
+        <h4 className="title_sub">Hello, I'm</h4>
+        <h1 className="title_main">Joshua Cheung</h1>
+        <h4 className="title_sub">Full-Stack Developer</h4>
+        <div className="imageContainer">
+          <img src={myPic} alt="" className="myPic" />
+        </div>
+        <div className="button_list">
+          <a href={Resume} download className="btn solid link">
+            resume
+          </a>
+          <a href="mailto:joshuacycheung@gmail.com" className="btn line link">
+            email me
+          </a>
+        </div>
       </div>
     </section>
   );
