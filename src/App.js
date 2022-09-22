d58a648aa693af7c40d4eff9eaa5bca899dca630
Joshua Cheung <contact.cjoshua@gmail.com>
2022-09-21 22:22:03 -0700

    fix for build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+
 import Footer from "./components/Footer";
 import { BrowserRouter } from "react-router-dom";
 
